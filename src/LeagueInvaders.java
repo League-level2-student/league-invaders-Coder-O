b6e6f89d54616cd20f551be87cba1d8ece4d1a84
Owen <owemarco@gmail.com>
2020-12-28 13:38:33 -0800

    Started LeagueInvaders
    I am doing it in eclipse, but it might need to be in Proccessing.

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,24 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame gameFrame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+	public static void main(String[] args) {
+		LeagueInvaders leagueInvadersInstance = new LeagueInvaders();
+		leagueInvadersInstance.setup();
+	}
+	
+	LeagueInvaders() {
+		gameFrame = new JFrame();
+		
+	}
+	
+	void setup() {
+		gameFrame.setSize(WIDTH, HEIGHT);
+		gameFrame.setVisible(true);
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
 }
